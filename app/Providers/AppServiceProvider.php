099511a9571645bf45681c2a363bca3cc5ba37d9
Kadek Pradnyana <kadekpradnyana@Kadeks-MacBook-Pro.local>
2019-03-18 12:04:26 +0800

    update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Purchase;
-use App\Mail\InvoiceShipped;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
@@ -18,20 +16,6 @@ class AppServiceProvider extends ServiceProvider
         if ( env('REDIRECT_HTTPS') ) {
             $url->formatScheme('https');
         }
-
-        Purchase::updated(function ($purchase) {
-            if ($purchase->status_payment == 'capture' && $purchase->isNotConfirm()) {
-                $purchase->completed = Purchase::CONFIRMED;
-                $purchase->save();
-
-                // send invoice
-                // retry sending email 5 times every 100 ms
-                retry(5, function() use ($purchase) {
-                    $dataInvoice = Purchase::with('user')->where('invoice_number', $purchase->invoice_number)->firstOrFail();
-                    Mail::to($dataInvoice->user)->send(new InvoiceShipped($dataInvoice));
-                }, 100);
-            }
-        });
     }
 
     public function register()
